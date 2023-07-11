12a39672e8eb5b713cca4cb88acbdfe3a8c2113f
Vijayakumar A <vijayakumar-ayyavoo_tmna>
2023-07-11 11:05:37 +0530

    org name cleanup

--- populate_action_usage.py
+++ populate_action_usage.py
@@ -1,44 +1,12 @@
 #!/usr/bin/python
 
+"""Gets the actions usage info and populates to Github Output
+"""
 import os
 import requests
-import argparse
 
-parser = argparse.ArgumentParser(
-    description="""
-        Gets the actions usage info and populates to Github Output
-        Usage:
-            python populate_action_usage.py --org 'my-org'
-            python populate_action_usage.py --user 'my-user'
 
-    """
-)
-
-parser.add_argument(
-    "-o",
-    "--org",
-    required=False,
-    help="The github org name to check for, ex: 'my-org'",
-)
-
-parser.add_argument(
-    "-u",
-    "--user",
-    required=False,
-    help="The github user to check for, ex: 'my-user'",
-)
-
-args = parser.parse_args()
-org_name = args.org
-user_name = args.user
-if not org_name and not user_name:
-    raise Exception(
-        f"Either org or user to be provided as input"
-    )
-
-github_access_token = os.environ['GH_TOKEN']
-
-def get_auth_header(access_token):
+def get_auth_header():
     """Gets the auth header
 
     Args:
@@ -47,7 +15,9 @@ def get_auth_header(access_token):
     Returns:
         dict: header
     """
+    access_token = os.environ["GH_TOKEN"]
     return {"Authorization": f"token {access_token}"}
+
 
 def get(url, headers=None):
     """Process get request
@@ -60,8 +30,9 @@ def get(url, headers=None):
         dict: response json
     """
     session = requests.session()
-    headers = get_auth_header(github_access_token)
-    session.headers.update(headers)
+    session.headers.update(get_auth_header())
+    if headers:
+        session.headers.update(headers)
     response = session.get(url)
     reponse_json = None
     if response.status_code not in [200]:
@@ -74,18 +45,28 @@ def get(url, headers=None):
         reponse_json = response.json()
     return reponse_json
 
-def get_usage_info_url():
-    """Gets actions minutes usage info API Url
+
+def get_owner_type():
+    """Gets the github repo owner type"""
+    org_repo_name = os.environ["GITHUB_REPOSITORY"]
+    url = f"https://api.github.com/repos/{org_repo_name}"
+    repo_info_response = get(url)
+    return repo_info_response["owner"]["type"]
+
+
+def get_actions_billing_url():
+    """Gets actions billing info API Url
 
     Returns:
-        str: GitHub API url
+        str: GitHub actions billing API url
     """
-    account_path = ''
-    if org_name:
-        account_path = f'orgs/{org_name}'
-    if user_name:
-        account_path = f'users/{user_name}'
-    return f'https://api.github.com/{account_path}/settings/billing/actions'
+    owner_type = get_owner_type()
+    owner_name = os.environ["GITHUB_REPOSITORY_OWNER"]
+    account_type = "users" if owner_type == "User" else "orgs"
+    return (
+        f"https://api.github.com/{account_type}/{owner_name}/settings/billing/actions"
+    )
+
 
 def set_output(name, value):
     """Sets github action output
@@ -97,10 +78,10 @@ def set_output(name, value):
     with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
         print(f"{name}={value}", file=fh)
 
-def populate_usage_info():
-    """Populates usage info
-    """
-    url = get_usage_info_url()
+
+def populate_actions_usage_info():
+    """Populates actions usage info"""
+    url = get_actions_billing_url()
     response = get(url)
     total_minutes_used = response["total_minutes_used"]
     included_minutes = response["included_minutes"]
@@ -108,10 +89,11 @@ def populate_usage_info():
     total_minutes_used = 1500.00
     usage_percentage = 0
     if included_minutes > 0:
-        usage_percentage = (total_minutes_used / included_minutes) * 100
-    set_output('TOTAL_MINUTES_USED', total_minutes_used)
-    set_output('TOTAL_PAID_MINUTES_USED', total_paid_minutes_used)
-    set_output('INCLUDED_MINUTES', included_minutes)
-    set_output('USAGE_PERCENTAGE', usage_percentage)
-    
-populate_usage_info()
+        usage_percentage = round((total_minutes_used / included_minutes) * 100)
+    set_output("TOTAL_MINUTES_USED", total_minutes_used)
+    set_output("TOTAL_PAID_MINUTES_USED", total_paid_minutes_used)
+    set_output("INCLUDED_MINUTES", included_minutes)
+    set_output("USAGE_PERCENTAGE", usage_percentage)
+
+
+populate_actions_usage_info()
